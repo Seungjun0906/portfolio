166daea8d881c42c8aae6ecd2fb3f1c5c2599de2
Jun <87404676+Seungjun0906@users.noreply.github.com>
2025-04-13 00:18:41 +0900

    반응형 스타일 적용을 위한 유틸성 함수 추가

---
+++ src/styles/utils/responsiveStyle.ts
@@ -0,0 +1,30 @@
+import { style, StyleRule } from "@vanilla-extract/css";
+import { mediaQuery } from "../tokens/breakpoints";
+
+type Responsive<T> = {
+  base?: T;
+  sm?: T;
+  md?: T;
+  lg?: T;
+  xl?: T;
+};
+
+export function responsiveStyle<T extends Record<string, any>>(
+  styles: Responsive<StyleRule>
+) {
+  const base = styles.base ?? {};
+  const queries: Record<string, any> = {};
+
+  if (styles.sm) queries[`@media`] = { [mediaQuery.sm]: styles.sm };
+  if (styles.md)
+    queries[`@media`] = { ...queries[`@media`], [mediaQuery.md]: styles.md };
+  if (styles.lg)
+    queries[`@media`] = { ...queries[`@media`], [mediaQuery.lg]: styles.lg };
+  if (styles.xl)
+    queries[`@media`] = { ...queries[`@media`], [mediaQuery.xl]: styles.xl };
+
+  return style({
+    ...base,
+    ...queries,
+  });
+}
