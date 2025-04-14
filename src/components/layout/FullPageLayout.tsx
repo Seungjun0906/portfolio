d9cffefdbe4a78043845bd6be8d1af4db66550c3
Jun <87404676+Seungjun0906@users.noreply.github.com>
2025-04-15 08:59:38 +0900

    스크롤 기능이 의도대로 동작하지는 않는 것 같아 제거

--- src/components/layout/FullPageLayout.tsx
+++ src/components/layout/FullPageLayout.tsx
@@ -5,36 +5,41 @@ import gsap from "gsap";
 
 export const FullPageLayout = ({ children }: { children: ReactNode }) => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const { lenis } = useScroll();
+  // const { lenis } = useScroll();
 
-  useEffect(() => {
-    if (!containerRef.current) return;
+  /**
+   * 의도대로 작동하지 않음
+   * 원인 파악까지 주석처리
+   */
 
-    const sections = gsap.utils.toArray<HTMLElement>(".section");
+  // useEffect(() => {
+  //   if (!containerRef.current) return;
 
-    sections.forEach((section) => {
-      ScrollTrigger.create({
-        trigger: section,
-        start: "top 90%",
-        end: "bottom 10%",
-        onEnter: () =>
-          lenis?.scrollTo(section, {
-            offset: 0,
-            duration: 1.0,
-            immediate: false,
-          }),
-        onEnterBack: () =>
-          lenis?.scrollTo(section, {
-            offset: 0,
-            duration: 1.0,
-          }),
-      });
-    });
+  //   const sections = gsap.utils.toArray<HTMLElement>(".section");
 
-    return () => {
-      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
-    };
-  }, [lenis]);
+  //   sections.forEach((section) => {
+  //     ScrollTrigger.create({
+  //       trigger: section,
+  //       start: "top 90%",
+  //       end: "bottom 10%",
+  //       onEnter: () =>
+  //         lenis?.scrollTo(section, {
+  //           offset: 0,
+  //           duration: 1.0,
+  //           immediate: false,
+  //         }),
+  //       onEnterBack: () =>
+  //         lenis?.scrollTo(section, {
+  //           offset: 0,
+  //           duration: 1.0,
+  //         }),
+  //     });
+  //   });
+
+  //   return () => {
+  //     ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+  //   };
+  // }, [lenis]);
 
   return <main ref={containerRef}>{children}</main>;
 };
